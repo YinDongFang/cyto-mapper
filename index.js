c839d872cbcc5fc4717f9ee4dbaa6ec02b037597
Yin <doyin@deloitte.com.cn>
2024-03-27 17:52:30 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,16 @@
+const getRandomInt = require('./getRandomInt')
+const getArraySum = require('./getArraySum')
+const deepCopy = require('./deepCopy')
+const stringToNumberArray = require('./stringToNumberArray')
+const print = require('./print')
+
+module.exports = () => {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  }
